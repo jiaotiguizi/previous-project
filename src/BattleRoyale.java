67e56c0b877b66ab138b2b43f9147a7850f5e884
RegenLied <yl8721a@student.american.edu>
2020-10-17 23:02:31 -0400

    updated done by..

--- src/BattleRoyale.java
+++ src/BattleRoyale.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 
 public class BattleRoyale {
     public static void main(String[] args){
-
-
+        ArrayList<String> myList_win = new ArrayList<String>();
+        ArrayList<String> myList_lose = new ArrayList<String>();
+        ArrayList<String> myList_tie = new ArrayList<String>();
         PrintWriter writer;
         try{
+
             System.out.println(System.getProperty("user.dir"));
             writer = new PrintWriter("results.txt");
             ArrayList<SuperHero> heroes = SuperHero.readDataFile("./SuperheroDataset.csv");
-            int count = 0;
             for(int i = 0; i < heroes.size(); i++){
                 for(int j = i + 1; j < heroes.size(); j++){
                     int damageA = heroes.get(i).attack(heroes.get(j));
@@ -20,22 +21,67 @@ public class BattleRoyale {
                     String AlignmentB = heroes.get(j).getAlignment();
 
                     if(damageA > damageB && !AlignmentA.equals(AlignmentB))
-                        writer.println(heroes.get(i).getName() + " wins");
+                        myList_win.add(heroes.get(i).getAlignment());
+
                     else if(damageB > damageA && !AlignmentB.equals(AlignmentA))
-                        writer.println(heroes.get(j).getName() + " wins");
-                    else if(damageA == damageB)
-                        writer.println("it was a tie");
-                    else
-                        writer.println("They have same Alignment");
+                        myList_lose.add(heroes.get(i).getAlignment());
+
+                    else if (damageB == damageA && !AlignmentB.equals(AlignmentA))
+                        myList_tie.add(heroes.get(i).getAlignment());
+                    myList_tie.add(heroes.get(j).getAlignment());
                 }
             }
+
+            int count_good = 0;
+            int count_bad = 0;
+            int count_neutral = 0;
+            int count_good_lose = 0;
+            int count_bad_lose = 0;
+            int count_neutral_lose = 0;
+            int count_good_tie = 0;
+            int count_bad_tie = 0;
+            int count_neutral_tie = 0;
+            for(int i=0; i < myList_win.size(); i++){
+                if (myList_win.get(i).equals("good"))
+                    count_good+=1;
+                else if (myList_win.get(i).equals("bad"))
+                    count_bad +=1;
+                else if (myList_win.get(i).equals("neutral"))
+                    count_neutral +=1;
+            }
+            for(int i=0; i < myList_lose.size(); i++){
+                if (myList_lose.get(i).equals("good"))
+                    count_good_lose+=1;
+                else if (myList_lose.get(i).equals("bad"))
+                    count_bad_lose +=1;
+                else if (myList_lose.get(i).equals("neutral"))
+                    count_neutral_lose +=1;
+            }
+            for(int i=0; i < myList_tie.size(); i++){
+                if (myList_tie.get(i).equals("good"))
+                    count_good_tie+=1;
+                else if (myList_tie.get(i).equals("bad"))
+                    count_bad_tie +=1;
+                else if (myList_tie.get(i).equals("neutral"))
+                    count_neutral_tie +=1;
+            }
+            writer.println("Good hero win: " + count_good);
+            writer.println("bad hero win: " + count_bad);
+            writer.println("neutral hero win: " + count_neutral);
+            writer.println(Math.max(count_good, count_bad));
+            writer.println("Good hero lose: " + count_good_lose);
+            writer.println("bad hero lose: " + count_bad_lose);
+            writer.println("neutral hero lose: " + count_neutral_lose);
+            writer.println("Good hero tie: " + count_good_tie);
+            writer.println("bad hero tie: " + count_bad_tie);
+            writer.println("neutral hero tie: " + count_neutral_tie);
+
             writer.close();
         }
 
         catch (Exception e){
             System.out.println("File write error");
         }
-
 
 
     }
